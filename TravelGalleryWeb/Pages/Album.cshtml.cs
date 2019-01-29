dac073c38bc8b8e83ef403d044dcaa97b2d3a6d2
totadehelle <alt3rnata@gmail.com>
2019-01-29 23:48:39 +0300

    re-made pages as Razor Pages

---
+++ TravelGalleryWeb/Pages/Album.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TravelGalleryWeb.Pages
+{
+    public class AlbumModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ TravelGalleryWeb/Pages/Contacts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TravelGalleryWeb.Pages
+{
+    public class ContactsModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
