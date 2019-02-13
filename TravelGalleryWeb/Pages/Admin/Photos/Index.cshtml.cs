8815074efff2cf1a61db56213daa03757e82d68e
totadehelle <alt3rnata@gmail.com>
2019-02-13 20:01:27 +0300

    13/02/2019 - added pagination on the photo managing page.

--- TravelGalleryWeb/Pages/Admin/Photos/Index.cshtml.cs
+++ TravelGalleryWeb/Pages/Admin/Photos/Index.cshtml.cs
@@ -13,24 +13,15 @@ namespace TravelGalleryWeb.Pages.Admin.Photos
     public class IndexModel : PageModel
     {
         private readonly ApplicationContext _context;
-
-        public List<Album> Albums;
-        
-        public List<SelectListItem> AlbumsList => Albums
-            .Select(album => new SelectListItem {
-                Value = album.Id.ToString(),
-                Text = album.Name
-            }).ToList();
-        
+ 
         public IList<DisplayPhoto> Photos { get;set; }
-
+        
+        public List<SelectListItem> AlbumsList; 
+        
         [BindProperty(SupportsGet = true)] public int Id { get; set; }
         public string AlbumName { get; set; } = "--";
-
-        private const int PageSize = 5;
-        private int StartFrom = 0;
-
-        public int PageCurrent { get; set; } = 1;
+        [BindProperty(SupportsGet = true)] public int PageNumber { get; set; }
+        private const int PageSize = 15;
         public int PagesTotal { get; set; }
 
         public IndexModel(ApplicationContext context)
@@ -40,21 +31,31 @@ namespace TravelGalleryWeb.Pages.Admin.Photos
 
         public async Task OnGetAsync()
         {
-            Albums = await _context.Albums.AsNoTracking().Select(a => a).OrderBy(a => a.Name).ToListAsync();
+            var albums = await _context.Albums.AsNoTracking().Select(a => new{Id = a.Id, Name = a.Name}).OrderBy(a => a.Name).ToListAsync();
+            
+            AlbumsList = albums
+                .Select(album => new SelectListItem {
+                    Value = album.Id.ToString(),
+                    Text = album.Name
+                }).ToList();
             
             ViewData["Albums"] = AlbumsList;
             Photos = new List<DisplayPhoto>();
 
-            if (Albums.Any())
+            if (albums.Any())
             {
-                if (Albums.All(a => a.Id != Id))
+                if (albums.All(a => a.Id != Id))
                 {
-                    Id = Albums[0].Id;
+                    Id = albums[0].Id;
                 }
 
-                AlbumName = Albums.FirstOrDefault(a => a.Id == Id).Name;
+                AlbumName = albums.FirstOrDefault(a => a.Id == Id).Name;
 
                 CalculatePages();
+
+                if (PageNumber < 1 || PageNumber > PagesTotal)
+                {
+                    PageNumber = 1;}
 
                 Photos = await _context.Photos.AsNoTracking().Where(p => p.AlbumId == Id).Join(_context.Albums,
                     p => p.AlbumId,
@@ -67,22 +68,8 @@ namespace TravelGalleryWeb.Pages.Admin.Photos
                         Comment = p.Comment,
                         Year = p.Year,
                         AlbumName = a.Name
-                    }).Skip(StartFrom).Take(PageSize).ToListAsync();
+                    }).Skip((PageNumber -1) * PageSize).Take(PageSize).ToListAsync();
             }
-        }
-
-        public async Task OnGetNextAsync()
-        {
-            StartFrom += PageSize;
-            await OnGetAsync();
-            PageCurrent++;
-        }
-        
-        public async Task OnGetPreviousAsync()
-        {
-            StartFrom -= PageSize;
-            await OnGetAsync();
-            PageCurrent--;
         }
 
         private void CalculatePages()
