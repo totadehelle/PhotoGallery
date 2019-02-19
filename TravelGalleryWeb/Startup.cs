b277f357bdc966e154259429cf5ace411853909f
totadehelle <alt3rnata@gmail.com>
2019-02-19 16:50:51 +0300

    19/02/2019 - adding better big size photo displaying (not finished)

--- TravelGalleryWeb/Startup.cs
+++ TravelGalleryWeb/Startup.cs
@@ -85,7 +85,7 @@ namespace TravelGalleryWeb
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             var cookiePolicyOptions = new CookiePolicyOptions
